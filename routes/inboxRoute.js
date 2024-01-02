e2932e2d94883a53857a09c780d3b411cf959a8b
Prohlad99 <prohlad.m99@gmail.com>
2024-01-02 13:07:25 +0600

    first commit

---
+++ routes/inboxRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { getInbox } = require("../controllers/inboxController");
+
+router.get("/", getInbox);
+
+module.exports = router;
